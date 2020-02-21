81549c1a017b494c891eb8f2900d258aaf76a46f
vagabundoo <evillamorm@gmail.com>
2020-02-21 15:45:46 +0100

    added form to add users

--- api.py
+++ api.py
@@ -10,7 +10,9 @@ db = client.get_database()
 users_coll = db["users"]
 conversations_coll = db["conversations"]
 
-@app.route('/user/create/<username>')
+# User creation
+
+@app.route('/user/create/<username>')   
 def addUser(username):
     """
     Creates a user and saves it into the DB users.
@@ -19,25 +21,42 @@ def addUser(username):
     Returns:
         string with username and user_id
     """
-    user_id = random.choice(range(1000))
     user = {"username": username}
     result = users_coll.insert_one(user)
-    return str(f'Created user: {username}, with id {result.inserted_id}')
-    #return dumps(users_coll.find_one({"username":username},{"_id":1})) 
-    #return f'Created user {username} with user_id: {user_id}'
-    # Could consider adding a way to check that the user_id has not been used before.
-    # user_id)
+    resp = str(f'Created user: {username}, with id {result.inserted_id}')
+    print(resp)
+    return resp
 
-# (GET) /chat/create
-# Purpose: Create a conversation to load messages
-# Params: An array of users ids [user_id]
-# Returns: chat_id
+@app.route('/user/input', methods=['GET', 'POST']) 
+def inputUserForm():
+    """
+    Provides a form in html, where user can submit new users to database
+    Returns:
+        string with username and user_id
+    """
+    if request.method == 'POST': #this block is only entered when the form is submitted
+        username = request.form.get('username')
+        user = {"username": username}
+        result = users_coll.insert_one(user)
+        resp = str(f'Created user: {username}, with id {result.inserted_id}')
+        print(resp)
+        return resp 
 
+    return '''<form method="POST">
+                  <h3> Provide a username to add to the database: <input type="text" name="username"></h3>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
 
-@app.route('/chat/create')
-def createChat(list_users):
-    participants = [e for e in user_id]
-    conversations_coll.insert_one({})
+# Chats
+
+@app.route('/chat/create/', methods=['GET', 'POST'])
+def createChat():
+    if request.method == 'GET'
+    users = request.args.get('list_users')
+    participants = users.split(",")
+    result = conversations_coll.insert_one({"participants":participants})
+    resp = f'Created chat with users {", ".join(participants)}, with id {result.inserted_id}'
+    return resp 
 
 
 app.run("0.0.0.0", 5002, debug=True)
